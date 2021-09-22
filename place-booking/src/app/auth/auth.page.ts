fc34fa5c533d02655d7df6448f0c29e44a6bf896
vinu01 <vinodkumar15190@gmail.com>
2021-09-23 01:26:26 +0530

    Auth Form
    Finishing the Auth Form

--- place-booking/src/app/auth/auth.page.ts
+++ place-booking/src/app/auth/auth.page.ts
@@ -12,6 +12,9 @@ import { AuthService } from './auth.service';
 })
 export class AuthPage implements OnInit {
   isLoading=false;
+  isLogin=true;
+
+
   constructor(private authService: AuthService, private router: Router,private loadingCtrl:LoadingController) {}
 
   ngOnInit() {}
@@ -31,7 +34,26 @@ export class AuthPage implements OnInit {
 
   }
 
+  onSwitchAuthMode(){
+    this.isLogin=!this.isLogin;
+
+  }
+
   onSubmit(form: NgForm){
-    console.log(form);
+    if(!form.valid){
+      return;
+    }
+
+    const email= form.value.email;
+    const password= form.value.password;
+    console.log(email, password);
+
+    if(this.isLogin){
+      // send the request to lgoin servers
+    }
+    else{
+      // send the request to Signup servers
+    }
+   
   }
 }
